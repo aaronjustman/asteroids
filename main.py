eca6a73ae6c3454ef04d2b16552125020433c658
AaronJ <aaron.justman@gmail.com>
2024-11-11 10:45:36 -0800

    Got rotation and moving working.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import *
+from constants import (SCREEN_HEIGHT, SCREEN_WIDTH)
+from player import Player
 
 
 def main():
@@ -11,13 +12,24 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill((0, 0, 0))
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
